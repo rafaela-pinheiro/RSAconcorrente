703d7613b3b82979d44912c027e8b8e93356a414
Amaral220x <biel.ilha2021@gmail.com>
2022-11-30 00:16:22 -0300

    Alterando a ortografia para decriptar

--- decriptar.c
+++ decriptar.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
 #include <math.h>
 #include <gmp.h>
 #include "timer.h"
 #include <time.h>
 
-char **blocos;
-int qnt;
+int nThreads;
+
+// Variáveis para tomada de tempo
 double inicio, fim, elapsed;
-mpz_t r;
+
+// Estrutura para os argumentos da thread
+typedef struct
+{
+    int id;
+    char *n;
+    char *d;
+} t_Args;
+
+char **gargv;
+int gargc;
+
 // Tabela de correspodência entre os caracteres e seus códigos
 char codSim[104] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', ' ', '9',
                     '=', '+', '-', '/', '*', 'a', 'b', 'c', ' ', 'd', 'e', 'f', 'g', 'h', 'i',
@@ -20,41 +33,35 @@ char codSim[104] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', ' ', '9',
                     '$', '%', '@', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
                     '\n', '{', '}'};
 
-void descriptarGMP(char *bloco, char *n, char *d)
+void decriptarGMP(char *bloco, char *n, char *d, mpz_t *r)
 {
+    // Para decriptar, recebe o bloco, o modulo e a chave privada. Também recebe r para ser o valor de retorno
+    // O GMP é uma biblioteca em C que permite fazer aritmética modular com números grandes em C, tratando todos como se fossem stings
     mpz_t b, N, D;
     mpz_init_set_str(b, bloco, 0);
     mpz_init_set_str(N, n, 0);
     mpz_init_set_str(D, d, 0);
-    mpz_powm(r, b, D, N);
+    mpz_powm(*r, b, D, N); // r = b^D (mod N), alterando por referência
 }
-char *lerArquivo(char *arquivo)
-{
-    FILE *arq;
-    char *texto;
-    int tamanho;
-    arq = fopen(arquivo, "r"); // Abertura do arquivo
-    if (arq == NULL)
-    {
-        printf("Erro ao abrir o arquivo\n");
-        exit(1);
-    }
-    fseek(arq, 0, SEEK_END);                            // Movendo o ponteiro pro final do arquivo
-    tamanho = ftell(arq);                               // Pegando a posição do ponteiro (nosso tamanho do arquivo)
-    fseek(arq, 0, SEEK_SET);                            // Voltando o ponteiro pro inicio do arquivo
-    texto = (char *)malloc(tamanho + 1 * sizeof(char)); // Alocando o espaço necessário
 
-    char c;
-    int i = 0;
-    while ((c = fgetc(arq)) != EOF)
+void *decriptarThread(void *arg)
+{
+    t_Args *args = (t_Args *)arg;
+    int id = args->id;
+    char *n, *d, **argumentos;
+    n = args->n;
+    d = args->d;
+    mpz_t r;
+    mpz_init(r);
+    for (int i = id; i < gargc; i += nThreads)
     {
-        texto[i] = c; // Armazena todos caracteres em nossa string até o fim do arquivo
-        i++;
+        decriptarGMP(gargv[i], n, d, &r);    // Descripta o bloco, passando o resultado para o &r
+        gargv[i] = mpz_get_str(NULL, 10, r); // Converte o mpz_t para string e salva no vetor de strings
     }
-    texto[i] = '\0'; //"Fecha" a string
-    fclose(arq);     // Fecha o arquivo
-    return texto;
+    free(arg);
+    pthread_exit(NULL);
 }
+
 char codigoParaSimbolo(int codigo)
 {
     int temp;
@@ -66,19 +73,38 @@ char codigoParaSimbolo(int codigo)
     }
 }
 
-void descriptar(char *n, char *d)
+char *lerArquivo(char *arquivo)
 {
-    mpz_init(r);
-    for (int i = 0; i < qnt; i++)
+    FILE *arq;
+    char *texto;
+    int tamanho;
+    arq = fopen(arquivo, "r"); // Abertura do arquivo
+    if (arq == NULL)
     {
-        descriptarGMP(blocos[i], n, d);
-        blocos[i] = mpz_get_str(NULL, 0, r);
+        printf("Erro ao abrir o arquivo");
+        exit(1);
     }
+    fseek(arq, 0, SEEK_END);                            // Movendo o ponteiro pro final do arquivo
+    tamanho = ftell(arq);                               // Pegando a posição do ponteiro (nosso tamanho do arquivo)
+    fseek(arq, 0, SEEK_SET);                            // Voltando o ponteiro pro inicio do arquivo
+    texto = (char *)malloc(tamanho + 1 * sizeof(char)); // Alocando o espaço necessário
+
+    char c;
+    int i = 0;
+    while ((c = fgetc(arq)) != EOF)
+    {
+        texto[i] = c;
+        i++;
+    }
+    texto[i] = '\0';
+    fclose(arq);
+    return texto;
 }
+
 void escreverArquivo()
 {
     FILE *arq;
-    arq = fopen("textoSaidaSeq.txt", "w");
+    arq = fopen("textoSaida.txt", "w");
     if (arq == NULL)
     {
         printf("Erro ao abrir o arquivo");
@@ -89,11 +115,11 @@ void escreverArquivo()
     // Recebe os três dígitos para convertê-los para a letra correspondente
     char str[3], simbolo;
     int digitos = 0;
-    for (int i = 0; i < qnt; i++)
+    for (int i = 0; i < gargc; i++)
     {
-        for (int j = 0; j < strlen(blocos[i]); j++)
+        for (int j = 0; j < strlen(gargv[i]); j++)
         {
-            str[digitos] = blocos[i][j];
+            str[digitos] = gargv[i][j];
             digitos++;
             if (digitos == 3)
             {
@@ -112,39 +138,71 @@ void escreverArquivo()
 int main(int argc, char *argv[])
 {
     char *n, *d;
-    GET_TIME(inicio);
-    if (argc < 2)
-    {
-        printf("Digite: %s <arquivo> \n", argv[0]);
-        return 1;
-    }
-    char *texto = lerArquivo(argv[1]);
-    int qntBlocos = 1;
 
-    // Conta a quantidade de blocos
+    // Inicia a contagem do tempo
+    GET_TIME(inicio);
+
+    if (argc < 3)
+    {
+        printf("Digite: %s <arquivo de entrada> <número de threads>\n", argv[0]);
+        return -1;
+    }
+
+    char *texto = lerArquivo(argv[1]);
+    nThreads = atoi(argv[2]);
+
+    pthread_t tid[nThreads];
+    t_Args *args;
+
+    int qntBlocos = 1;
     for (int i = 0; i < strlen(texto); i++)
     {
+        // Calcula a quantidade de blocos
         if (texto[i] == ' ')
+        {
             qntBlocos++;
+        }
     }
-    qntBlocos -= 2; // Retira os dois primeiros blocos (n e d)
-    qnt = qntBlocos;
-    char *b[qntBlocos];
+    qntBlocos -= 2;
+
+    // Separa a string com todos os blocos em um vetor de strings (blocos)
+    char *strings[qntBlocos];
     int j = 0;
     n = strtok(texto, " ");
     d = strtok(NULL, " ");
-    b[j] = strtok(NULL, " ");
-    while (b[j] != NULL)
+    strings[j] = strtok(NULL, " ");
+    while (strings[j] != NULL)
     {
         j++;
-        b[j] = strtok(NULL, " ");
+        strings[j] = strtok(NULL, " "); // Equivalente ao .split() em java, separa a string até o determinado ponto
     }
-    blocos = b;
-    descriptar(n, d);
-    for(int i = 0; i < qnt; i++)
-        printf("%s\n", blocos[i]);
+    gargv = strings;
+    gargc = qntBlocos;
+
+    // Cria as threads
+    for (int i = 0; i < nThreads; i++)
+    {
+        args = malloc(sizeof(t_Args));
+        args->id = i;
+        args->n = n;
+        args->d = d;
+        pthread_create(&tid[i], NULL, decriptarThread, (void *)args);
+    }
+
+    // Espera todas as threads terminarem
+    for (int i = 0; i < nThreads; i++)
+    {
+        pthread_join(tid[i], NULL);
+    }
+    for (int i = 0; i < gargc; i++)
+    {
+        printf("DESCRIPTADO: %s\n", gargv[i]);
+    }
     escreverArquivo();
+
+    // Finaliza a contagem do tempo
     GET_TIME(fim);
-    printf("Tempo de execução: %lf\n", fim - inicio);
+    elapsed = fim - inicio;
+    printf("\nO codigo demorou %f segundos\n", elapsed);
     return 0;
 }
---
+++ decriptarSequencial.c
@@ -0,0 +1,172 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <gmp.h>
+#include "timer.h"
+#include <time.h>
+
+char **blocos;
+int qnt;
+double inicio, fim, tEntrada, tSaida, tProcess;
+
+mpz_t r;
+// Tabela de correspodência entre os caracteres e seus códigos
+char codSim[104] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', ' ', '9',
+                    '=', '+', '-', '/', '*', 'a', 'b', 'c', ' ', 'd', 'e', 'f', 'g', 'h', 'i',
+                    'j', 'k', 'l', ' ', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', ' ', 'v',
+                    'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', ' ', 'E', 'F', 'G', 'H', 'I', 'J',
+                    'K', 'L', 'M', ' ', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', ' ', 'W',
+                    'X', 'Y', 'Z', ',', '.', '!', '?', ';', ' ', ':', '_', '(', ')', '\'', '#',
+                    '$', '%', '@', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+                    '\n', '{', '}'};
+
+void decriptarGMP(char *bloco, char *n, char *d)
+{
+    mpz_t b, N, D;
+    mpz_init_set_str(b, bloco, 0);
+    mpz_init_set_str(N, n, 0);
+    mpz_init_set_str(D, d, 0);
+    mpz_powm(r, b, D, N);
+}
+char *lerArquivo(char *arquivo)
+{
+    FILE *arq;
+    char *texto;
+    int tamanho;
+    arq = fopen(arquivo, "r"); // Abertura do arquivo
+    if (arq == NULL)
+    {
+        printf("Erro ao abrir o arquivo\n");
+        exit(1);
+    }
+    fseek(arq, 0, SEEK_END);                            // Movendo o ponteiro pro final do arquivo
+    tamanho = ftell(arq);                               // Pegando a posição do ponteiro (nosso tamanho do arquivo)
+    fseek(arq, 0, SEEK_SET);                            // Voltando o ponteiro pro inicio do arquivo
+    texto = (char *)malloc(tamanho + 1 * sizeof(char)); // Alocando o espaço necessário
+
+    char c;
+    int i = 0;
+    while ((c = fgetc(arq)) != EOF)
+    {
+        texto[i] = c; // Armazena todos caracteres em nossa string até o fim do arquivo
+        i++;
+    }
+    texto[i] = '\0'; //"Fecha" a string
+    fclose(arq);     // Fecha o arquivo
+    return texto;
+}
+char codigoParaSimbolo(int codigo)
+{
+    int temp;
+    for (int i = 0; i < 104; i++)
+    {
+        temp = 111 + i;
+        if (codigo == temp)
+            return codSim[i];
+    }
+}
+
+void decriptar(char *n, char *d)
+{
+    mpz_init(r);
+    for (int i = 0; i < qnt; i++)
+    {
+        decriptarGMP(blocos[i], n, d);
+        blocos[i] = mpz_get_str(NULL, 0, r);
+    }
+}
+void escreverArquivo()
+{
+    FILE *arq;
+    arq = fopen("textoSaidaSeq.txt", "w");
+    if (arq == NULL)
+    {
+        printf("Erro ao abrir o arquivo");
+        exit(1);
+    }
+
+    /* Conversão de código para símbolo */
+    // Recebe os três dígitos para convertê-los para a letra correspondente
+    char str[3], simbolo;
+    int digitos = 0;
+    for (int i = 0; i < qnt; i++)
+    {
+        for (int j = 0; j < strlen(blocos[i]); j++)
+        {
+            str[digitos] = blocos[i][j];
+            digitos++;
+            if (digitos == 3)
+            {
+                // Se digitos == 3, já temos um código completo e podemos converter
+                simbolo = codigoParaSimbolo(atoi(str));
+                // Escreve o char no arquivo
+                fputc(simbolo, arq);
+                digitos = 0;
+            }
+        }
+    }
+
+    fclose(arq);
+}
+
+int main(int argc, char *argv[])
+{
+    char *n, *d;
+
+    // tempo de entrada
+    GET_TIME(inicio);
+    if (argc < 2)
+    {
+        printf("Digite: %s <arquivo> \n", argv[0]);
+        return 1;
+    }
+    char *texto = lerArquivo(argv[1]);
+    int qntBlocos = 1;
+
+    // Conta a quantidade de blocos
+    for (int i = 0; i < strlen(texto); i++)
+    {
+        if (texto[i] == ' ')
+            qntBlocos++;
+    }
+    qntBlocos -= 2; // Retira os dois primeiros blocos (n e d)
+    qnt = qntBlocos;
+    char *b[qntBlocos];
+    int j = 0;
+    n = strtok(texto, " ");
+    d = strtok(NULL, " ");
+    b[j] = strtok(NULL, " ");
+    while (b[j] != NULL)
+    {
+        j++;
+        b[j] = strtok(NULL, " ");
+    }
+    blocos = b;
+    GET_TIME(fim);
+
+    tEntrada = fim - inicio;
+
+    // tempo de processamento
+    GET_TIME(inicio);
+
+    decriptar(n, d);
+
+    GET_TIME(fim);
+
+    tProcess = fim - inicio;
+
+    // tempo de saída
+    GET_TIME(inicio);
+    for (int i = 0; i < qnt; i++)
+        printf("DESCRIPTADO: %s\n", blocos[i]);
+
+    escreverArquivo();
+
+    GET_TIME(fim);
+
+    tSaida = fim - inicio;
+
+    printf("Tempos de execução:\nEntrada: %lf\nProcessamento: %lf\nSaida: %lf\nTotal: %lf\n", tEntrada, tProcess, tSaida, tEntrada + tProcess + tSaida);
+    return 0;
+}
